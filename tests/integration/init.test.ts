7c2b9a099ab4d037ba76e8317b28f98cb916c0c9
compota334 <compota334@gmail.com>
2026-05-01 21:53:52 +0200

    Arreglar test flaky de init usando GUARD_ANGELS_ANGEL_CMD=/bin/false para forzar backend no disponible

--- tests/integration/init.test.ts
+++ tests/integration/init.test.ts
@@ -168,6 +168,7 @@ describe('angels init', () => {
         sanitizedEnv[key] = value;
       }
     }
+    sanitizedEnv['GUARD_ANGELS_ANGEL_CMD'] = '/bin/false';
 
     const result = await execaNode(CLI_PATH, ['init', '--auto'], {
       cwd: tmpDir,
